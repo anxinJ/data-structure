25a9137d19e3c89b1b7d9291674f5c94150e716d
anxinJ <1028361767@qq.com>
2017-06-14 16:10:26 +0800

    ShortestWord :complete and commit the best-practices solution.

--- src/codewars/kyu6/ShortestWord.java
+++ src/codewars/kyu6/ShortestWord.java
@@ -1,12 +1,23 @@
 package codewars.kyu6;
 
+
+import java.util.stream.Stream;
+
 /**
  * Created by hewj on 17/6/14.
  */
 public class ShortestWord {
 
     public static int findShort(String s) {
+        int minLen = s.length();
+        String[] words = s.split(" ");
+        for(String word : words){
+            if(word.length() < minLen) minLen = word.length();
+        }
+        return minLen;
+    }
 
-        return 0;
+    public static int bestPractices(String s){
+        return Stream.of(s.split(" ")).mapToInt(String::length).min().getAsInt();
     }
 }
